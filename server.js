b7e0dd3ede56359e3814167daadd246c9e1ed28c
Scott Christensen <scottmc526@gmail.com>
2016-01-04 14:39:18 -0700

    attempting to add 1337

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 
+function indexPg (req,res) {
+    fs.readFile('./index.html', function (err, data){
+      if (err) {
+        console.log('no');
+      } else {
+        res.setHeader('Content-Type','text/html')
+        res.statusCode = 200;
+        res.write(data);
+        res.end()
+      }
+    })
+}
+
 function handleRequest (req, res) {
   if (req.url === '/about.html') {
     fs.readFile('./about.html', function (err, data){
@@ -34,7 +47,12 @@ function handleRequest (req, res) {
 }
 
 var server = http.createServer(handleRequest);
+var indexServer = http.createServer(indexPg);
 
 server.listen(8000, function() {
   console.log("I'm listening on port 8000...")
 });
+
+indexServer.listen(1337, function (){
+  console.log("I'm listening on port 1337");
+})
